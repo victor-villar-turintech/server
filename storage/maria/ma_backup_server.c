97d105b46435c05762e17146f1b3c320007feae5
Marko Mäkelä <marko.makela@mariadb.com>
2026-09-04 09:39:34 +0300

    Diagnose a failure to copy aria_log_control

--- storage/maria/ma_backup_server.c
+++ storage/maria/ma_backup_server.c
@@ -272,7 +272,13 @@ static int aria_backup_file(const struct backup_target *target,
         ret= 0;
       else
       {
+# if 1 // TODO: remove this; trying to diagnose the exact error
+        DWORD err= GetLastError();
+        sql_print_error("can't create %s: %lu", dstpath, err);
+        my_osmaperr(err);
+# else
         my_osmaperr(GetLastError());
+# endif
         my_error(ER_CANT_CREATE_FILE, MYF(0), dstpath, errno);
       }
       free(dstpath);
